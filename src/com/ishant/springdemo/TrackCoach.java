c72d091edd6de1707e77018e151fdac6f107156d
IshantL <ishantlambhate@gmail.com>
2018-08-21 09:25:17 +0000

    Spring Bean Lifecycle Hooks

--- src/com/ishant/springdemo/TrackCoach.java
+++ src/com/ishant/springdemo/TrackCoach.java
@@ -19,4 +19,12 @@ public class TrackCoach implements Coach {
 		return "Just do it " + fortuneService.getFortune();
 	}
 
+	//add an init method
+	public void doMyStartupStuff(){
+		System.out.println("TrackCoach:inside methodStartupStuff");
+	}
+	//add a destroy method
+	public void doMyCleanupStuff(){
+		System.out.println("TrackCoach:inside methodDestroyStuff");
+	}
 }
