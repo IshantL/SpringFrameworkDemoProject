7fd3e9f7e1c4feefc3bdb052bc8232d182c4eada
ishantVL <ishant.lambhate@tcs.com>
2018-08-21 13:00:11 +0530

    DI by Injecting Literal values and from Property file

--- src/com/ishant/springdemo/CricketCoach.java
+++ src/com/ishant/springdemo/CricketCoach.java
@@ -3,13 +3,13 @@ package com.ishant.springdemo;
 public class CricketCoach implements Coach {
 
 	FortuneService fortuneService;
+	private String email;
+	private String team;
 	
 	//create a new argument constructor
 	public CricketCoach() {
 		System.out.println("Cricket Coach :Inside no-arg constructor");
 	}
-
-	
 
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("Cricket Coach :Inside the setter method");
@@ -17,6 +17,23 @@ public class CricketCoach implements Coach {
 	}
 
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		System.out.println("Cricket Coach :Inside the setter method of email");
+		this.email = email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("Cricket Coach :Inside the setter method of team");
+		this.team = team;
+	}
 
 	public String getDailyWorkout() {
 		return "Prctise fast bowling";
--- src/com/ishant/springdemo/SetterDemo.java
+++ src/com/ishant/springdemo/SetterDemo.java
@@ -9,6 +9,10 @@ public class SetterDemo {
 		//retrive bans,call methods, close context
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		CricketCoach coach= context.getBean("myCricketCoach",CricketCoach.class);
+		System.out.println(coach.getDailyWorkout());
+		System.out.println(coach.getDailyFortune());
+		System.out.println(coach.getEmail());
+		System.out.println(coach.getTeam());
 		context.close();
 	}
 
